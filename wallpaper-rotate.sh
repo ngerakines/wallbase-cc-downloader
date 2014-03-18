2c891abed345a627f256dc034d4933fd6b6f5cc8
Nick Gerakines <nick@gerakines.net>
2014-03-18 15:58:29 -0400

    Adding log and disable to rotate script.

--- wallpaper-rotate.sh
+++ wallpaper-rotate.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-pcmanfm --set-wallpaper="$(find /path/to/your/wallpapers/ -name *jpg -type f | shuf -n1)"
+
+DISABLE_FILE="/path/to/.wallpaper-rotate-disable"
+LOG_FILE=/path/to/.wallpaper-rotate.log
+
+if [ -f "$DISABLE_FILE" ]
+then
+    exit
+fi
+
+
+WALLPAPER=$(find /path/to/your/wallpapers/ -name *jpg -type f | shuf -n1)
+pcmanfm --set-wallpaper="$WALLPAPER"
+
+echo "$(date) $WALLPAPER" >> $LOG_FILE
